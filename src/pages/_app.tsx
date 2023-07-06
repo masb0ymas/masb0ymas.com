0a9d7fb532c6d638c1c2358bc7fe3edb6655cec4
masb0ymas <n.fajri@outlook.com>
2023-07-06 19:28:37 +0700

    fix: update page _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,11 +30,29 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
 
   const siteLayout = getSiteLayout(props)
 
+  const title = BRAND
+  const description = `${BRAND} - Open source software for web development`
+  const metaURL = 'https://masb0ymas.com'
+  const metaImage = '/static/images/logo.png'
+  const webIconURL = '/static/images/logo.png'
+
   return (
     <>
       <Head>
         <title>{`${BRAND} Stack`}</title>
-        <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+        <meta name="title" content={title} />
+        <link rel="icon" href={webIconURL} />
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metaURL} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={metaImage} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={metaURL} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={metaImage} />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
