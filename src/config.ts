dd08828f3639372cfdd2675aac9f2c15d30c422f
masb0ymas <n.fajri@outlook.com>
2024-07-15 10:03:57 +0700

    fix: change avatar and description web

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "masb0ymas";
-export const SITE_DESCRIPTION = "I write a blog for my note";
+export const SITE_DESCRIPTION = "I write blog articles about web development and web implementation covering Web2, Web3, and DevOps";
 export const TWITTER_HANDLE = "@masb0ymas";
 export const MY_NAME = "N. Fajri";
 export const MY_POSITION = "Software Engineer";
