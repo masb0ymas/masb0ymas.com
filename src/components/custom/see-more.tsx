a1f5ab8339b5d20df63ab7db44fbe8ad0c7d1782
masb0ymas <n.fajri@outlook.com>
2025-04-26 22:54:24 +0700

    feat: add see-more ui

---
+++ src/components/custom/see-more.tsx
@@ -0,0 +1,16 @@
+import { IconArrowRight } from '@tabler/icons-react'
+import Link from 'next/link'
+
+type SeeMoreProps = {
+  label: string
+  link: string
+}
+
+export default function SeeMore({ label, link }: SeeMoreProps) {
+  return (
+    <Link href={link} className="flex flex-row items-center gap-2 hover:underline">
+      <span className="text-neutral-400 dark:text-neutral-200">{label}</span>
+      <IconArrowRight className="h-6 w-6 text-neutral-400 dark:text-neutral-200" />
+    </Link>
+  )
+}
