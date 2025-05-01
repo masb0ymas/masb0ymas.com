bd7a80c4ec24213e4055bea1b5b1211cf2737310
masb0ymas <n.fajri@outlook.com>
2025-05-01 23:32:04 +0700

    feat: add blog slug with render mdx content

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,20 @@
+type IParams = {
+  slug: string
+}
+
+type IProps = {
+  params: Promise<IParams>
+}
+
+export default async function BlogDetailPage({ params }: IProps) {
+  const { slug } = await params
+  const { default: Post } = await import(`~/contents/blog/${slug}.mdx`)
+
+  return (
+    <section className="mx-auto max-w-5xl px-4 py-10 md:px-6 lg:pt-0 lg:pb-20">
+      <article className="prose lg:prose-lg max-w-full">
+        <Post />
+      </article>
+    </section>
+  )
+}
--- src/app/home/blog.tsx
+++ src/app/home/blog.tsx
@@ -1,36 +1,7 @@
 import CardPost from '~/components/custom/card-post'
 import SeeMore from '~/components/custom/see-more'
 import { Separator } from '~/components/ui/separator'
-
-const posts = [
-  {
-    title: 'How to Use GitHub Actions for CI/CD and Push to Google Artifact Registry',
-    description:
-      'A guide to setting up a CI/CD workflow using GitHub Actions to automate the build and deployment process and push artifacts to Google Artifact Registry.',
-    slug: '/blog/how-to-use-github-actions-for-ci-cd-and-push-to-google-artifact-registry',
-    thumbnail: '/static/blog/ci-cd-github-actions-gcp/github_action_build_success.webp',
-    date: '2024-11-04',
-    tags: ['Docker', 'CI/CD', 'GitHub Actions'],
-  },
-  {
-    title: 'How to Install React Native on Different Platforms?',
-    description:
-      'How to Install React Native on Different Platforms? This guide provides a step-by-step tutorial on installing React Native across various platforms, including Windows, macOS, and Linux.',
-    slug: '/blog/how-to-install-react-native-on-different-platforms',
-    thumbnail: '/static/blog/ci-cd-github-actions-gcp/github_action_build_success.webp',
-    date: '2024-10-24',
-    tags: ['React Native', 'CLI'],
-  },
-  {
-    title: 'How to Use Environment Variables in Sveltekit',
-    description:
-      'This blog post provides a comprehensive guide on using environment variables in SvelteKit to manage configurations and sensitive data securely.',
-    slug: '/blog/how-to-use-environment-variables-in-sveltekit',
-    thumbnail: '/static/blog/sveltekit/svelte.webp',
-    date: '2024-10-14',
-    tags: ['SvelteKit', 'Environment Variables'],
-  },
-]
+import { posts } from '~/data/posts.json'
 
 export default function FeaturedPosts() {
   return (
@@ -45,7 +16,7 @@ export default function FeaturedPosts() {
       </div>
 
       <div className="mt-10 flex flex-col gap-4">
-        {posts.map((post, index) => (
+        {posts.slice(0, 3).map((post, index) => (
           <div key={index}>
             <CardPost {...post} />
             {index !== posts.length - 1 && <Separator className="mt-4" />}
