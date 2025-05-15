11d3028896c29fafcddd6cd66abdb437fbf6072f
masb0ymas <n.fajri@outlook.com>
2025-05-15 22:10:58 +0700

    fix: move syntax highlighting to components

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 import SyntaxHighlighting from '~/components/custom/syntax-highlighting'
-import postsJson from '~/data/posts.json'
+import projectsJson from '~/data/projects.json'
 
 type IParams = {
   slug: string
@@ -12,33 +12,33 @@ type IProps = {
 
 export async function generateMetadata({ params }: IProps): Promise<Metadata> {
   const { slug } = await params
-  const post = postsJson.posts.find((post) => post.slug === `/blog/${slug}`)
-  const tags = post?.tags || []
+  const project = projectsJson.projects.find((project) => project.slug === `/project/${slug}`)
+  const tags = project?.tags || []
 
   return {
-    title: post?.title,
-    description: post?.description,
-    keywords: [...tags, 'Tech', 'Web', 'AI', 'Web3', 'masb0ymas', 'masb0ymas blog'],
+    title: project?.title,
+    description: project?.description,
+    keywords: [...tags, 'Tech', 'Web', 'AI', 'Web3', 'masb0ymas', 'masb0ymas project'],
     openGraph: {
-      title: post?.title,
-      description: post?.description,
+      title: project?.title,
+      description: project?.description,
     },
     twitter: {
-      title: post?.title,
-      description: post?.description,
+      title: project?.title,
+      description: project?.description,
     },
   }
 }
 
-export default async function BlogDetailPage({ params }: IProps) {
+export default async function ProjectDetailPage({ params }: IProps) {
   const { slug } = await params
-  const { default: Post } = await import(`~/contents/blog/${slug}.mdx`)
+  const { default: Project } = await import(`~/contents/project/${slug}.mdx`)
 
   return (
     <section className="mx-auto max-w-5xl px-4 py-10 md:px-6 lg:pt-0 lg:pb-20">
       <article className="prose lg:prose-lg max-w-full">
         <SyntaxHighlighting>
-          <Post />
+          <Project />
         </SyntaxHighlighting>
       </article>
     </section>
