f94ff2f0c08153bfd77ba8f7e770e948fa2dd721
masb0ymas <n.fajri@outlook.com>
2025-09-06 22:30:22 +0700

    fix: update home hero

--- src/app/(home)/partials/hero.tsx
+++ src/app/(home)/partials/hero.tsx
@@ -1,48 +1,4 @@
-import {
-  IconBrandBluesky,
-  IconBrandGithub,
-  IconBrandLinkedin,
-  IconBrandX,
-  IconFileSpark,
-  IconMail,
-} from '@tabler/icons-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip'
-
-const socials = [
-  {
-    name: 'X',
-    href: 'https://x.com/masb0ymas',
-    icon: IconBrandX,
-  },
-  {
-    name: 'Bluesky',
-    href: 'https://bsky.app/profile/masb0ymas.bsky.social',
-    icon: IconBrandBluesky,
-  },
-  {
-    name: 'LinkedIn',
-    href: 'https://linkedin.com/in/masb0ymas',
-    icon: IconBrandLinkedin,
-  },
-  {
-    name: 'GitHub',
-    href: 'https://github.com/masb0ymas',
-    icon: IconBrandGithub,
-  },
-  {
-    name: 'Email',
-    href: 'mailto:me@masb0ymas.com',
-    icon: IconMail,
-  },
-  {
-    name: 'Resume',
-    href: 'https://drive.google.com/file/d/1An3Sb1dhqLFqtNt7fXvf5qK0HN_vovLV/view?usp=sharing',
-    icon: IconFileSpark,
-  },
-]
+import Profile from '~/components/common/profile'
 
 export default function Hero() {
   return (
@@ -68,60 +24,9 @@ export default function Hero() {
           </div>
         </div>
         <div className="col-span-full h-full lg:col-span-1">
-          <div className="flex flex-col items-center justify-center gap-2">
-            <AvatarCard>
-              <Image
-                src="/static/images/profile.jpeg"
-                alt="Profile"
-                width={512}
-                height={512}
-                className="relative mx-auto w-full rounded-lg shadow backdrop-blur-xl md:w-1/2 dark:bg-zinc-900/90"
-              />
-            </AvatarCard>
-
-            <div className="flex flex-row gap-4">
-              {socials.map((social) => (
-                <TooltipProvider key={social.name}>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Link
-                        key={social.name}
-                        href={social.href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cursor-newtab"
-                      >
-                        <social.icon className="text-primary-200 dark:text-primary-100/60 h-6 w-6" />
-                      </Link>
-                    </TooltipTrigger>
-                    <TooltipContent>{social.name}</TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              ))}
-            </div>
-
-            {/* <div className="flex items-center justify-center gap-2 mt-2">
-              <DotPulse />
-              <span className="text-primary-200 dark:text-primary-100/60 font-bold">Available</span>
-            </div> */}
-          </div>
+          <Profile available={false} />
         </div>
       </div>
     </section>
-  )
-}
-
-interface CardProps {
-  children: React.ReactNode
-}
-
-const AvatarCard = ({ children }: CardProps) => {
-  return (
-    <div className="py-10">
-      <div className="relative w-full">
-        <div className="absolute inset-0 mx-auto w-full scale-x-95 -rotate-[5deg] rounded-lg bg-gray-200 py-10 md:w-1/2 dark:bg-zinc-800" />
-        {children}
-      </div>
-    </div>
   )
 }
