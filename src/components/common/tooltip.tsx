fcdc501bb37754f06e17a68d1a32b2151321aeac
masb0ymas <n.fajri@outlook.com>
2025-09-06 22:29:58 +0700

    feat: add common tooltip

---
+++ src/components/common/tooltip.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
+
+interface CommonTooltipProps {
+  children: React.ReactNode
+  content: string
+}
+
+export default function CommonTooltip({ children, content }: CommonTooltipProps) {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipContent>{content}</TooltipContent>
+    </Tooltip>
+  )
+}
