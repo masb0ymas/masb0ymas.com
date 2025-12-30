0d96894e2f6957c526a1b6e1fc22f48ee0e22523
masb0ymas <n.fajri@outlook.com>
2025-12-30 12:26:43 +0700

    fix: update github open source

--- src/app/(home)/partials/github-os.tsx
+++ src/app/(home)/partials/github-os.tsx
@@ -6,6 +6,7 @@ import { useTheme } from 'next-themes'
 
 import SectionTitle from '~/components/common/section-title'
 import SeeMore from '~/components/common/see-more'
+import { useMemo } from 'react'
 
 export default function GithubOS() {
   const { theme } = useTheme()
@@ -15,42 +16,46 @@ export default function GithubOS() {
   const darkTheme =
     'bg_color=181D27&title_color=fb923c&text_color=FAFAFA&border_color=535862&icon_color=fb923c&border_radius=10'
 
+  const currentThemes = useMemo(() => {
+    return theme === 'light' ? lightTheme : darkTheme
+  }, [theme])
+
   const reviews = [
     {
       id: 1,
       title: 'express-api-typeorm',
       repository_url: 'https://github.com/masb0ymas/express-api-typeorm',
-      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=express-api-typeorm&${theme === 'light' ? lightTheme : darkTheme}`,
+      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=express-api-typeorm&${currentThemes}`,
     },
     {
       id: 2,
       title: 'express-api-sequelize',
       repository_url: 'https://github.com/masb0ymas/express-api-sequelize',
-      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=express-api-sequelize&${theme === 'light' ? lightTheme : darkTheme}`,
+      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=express-api-sequelize&${currentThemes}`,
     },
     {
       id: 3,
       title: 'express-api',
       repository_url: 'https://github.com/masb0ymas/express-api',
-      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=express-api&${theme === 'light' ? lightTheme : darkTheme}`,
+      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=express-api&${currentThemes}`,
     },
     {
       id: 4,
       title: 'gofi',
       repository_url: 'https://github.com/masb0ymas/gofi',
-      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=gofi&${theme === 'light' ? lightTheme : darkTheme}`,
+      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=gofi&${currentThemes}`,
     },
     {
       id: 5,
-      title: 'create-expressjs-starterkit',
-      repository_url: 'https://github.com/masb0ymas/create-expressjs-starterkit',
-      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=create-expressjs-starterkit&${theme === 'light' ? lightTheme : darkTheme}`,
+      title: 'gintama',
+      repository_url: 'https://github.com/masb0ymas/gintama',
+      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=gintama&${currentThemes}`,
     },
     {
       id: 6,
-      title: 'go-utils',
-      repository_url: 'https://github.com/masb0ymas/go-utils',
-      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=go-utils&${theme === 'light' ? lightTheme : darkTheme}`,
+      title: 'audiopintar',
+      repository_url: 'https://github.com/masb0ymas/audiopintar',
+      gsr_url: `https://grs.masb0ymas.com/api/pin/?username=masb0ymas&repo=audiopintar&${currentThemes}`,
     },
   ]
 
