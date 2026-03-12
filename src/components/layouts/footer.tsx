e857e23d19cbbd1279bcbc7bbb64bfeaba5840da
masb0ymas <n.fajri@outlook.com>
2026-03-12 11:25:35 +0700

    fix: update footer, privacy and terms page

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { env } from '~/env'
 
 export default function Footer() {
@@ -5,10 +7,20 @@ export default function Footer() {
     <footer className="border-t bg-neutral-900 py-4">
       <div className="mx-auto max-w-7xl px-4 md:px-6">
         <div className="flex items-center justify-center">
-          <p className="text-sm text-neutral-400 lg:text-base">
-            Copyright &copy; {new Date().getFullYear()} {env.NEXT_PUBLIC_APP_NAME}. All rights
-            reserved.
-          </p>
+          <div className="flex flex-row items-center gap-2">
+            <Link className="text-muted-foreground text-sm" href="/privacy">
+              Privacy
+            </Link>
+            <span className="text-sm text-neutral-700">•</span>
+            <Link className="text-muted-foreground text-sm" href="/terms">
+              Terms
+            </Link>
+            <span className="text-sm text-neutral-700">•</span>
+            <p className="text-sm text-neutral-400">
+              Copyright &copy; {new Date().getFullYear()} {env.NEXT_PUBLIC_APP_NAME}. All rights
+              reserved.
+            </p>
+          </div>
         </div>
       </div>
     </footer>
